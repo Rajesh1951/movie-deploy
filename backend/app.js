2c244bf5063de9ac6e2c6a5e65514aa758443903
Rajesh1951 <rajesh2oo1code@gmail.com>
2023-07-23 23:30:08 +0530

    vercel server

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config();
-const axios = require('axios')
 const app = express();
 app.use(express.json());
 const router = require('./routes/appRoutes')
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: "*",
+  methods: ["GET"],
+  credentials: true
 };
 
 app.use(cors(corsOptions));
